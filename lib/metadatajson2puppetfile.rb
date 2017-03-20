093a62b653ec0e98b522ef74ff249e2f04e49ca2
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-03-21 01:53:44 +1100

    initial

--- lib/metadatajson2puppetfile.rb
+++ lib/metadatajson2puppetfile.rb
@@ -5,17 +5,17 @@ module Metadatajson2puppetfile
   def self.run
     if File.exists?(METADATA)
       begin
-        data = JSON.parse(File.readlines(filename))
-        data["dependencies"].each { |dep|
-          if dep["version_requirement"] =~ /\d+\.\d+\.\d+/
-            version_string = dep["version_requirement"]
+        data = JSON.parse(File.read(METADATA))
+        puts "# dependences for #{data['name']}"
+        data["dependencies"].each { |pair|
+          if pair["version_requirement"] =~ /^\d+\.\d+\.\d+$/
+            version_string = pair["version_requirement"]
           else
             version_string = ":latest"
           end
-          dep_string = "mod '#{dep['name']}', '#{version_string}'"
+          dep_string = "mod '#{pair['name']}', '#{version_string}'"
           puts dep_string
         }
-        puts "done"
       rescue JSON::ParserError
         raise "syntax error in #{METADATA}"
       end
